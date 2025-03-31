61a7d9ec2ef2cc66f902f14b62fb77dbfef2366a
Goldman <goldmanmine@mail.ru>
2025-03-31 07:32:09 +0300

    lab 4

--- compileL4.bat
+++ compileL4.bat
@@ -1,12 +1,18 @@
 set folder=lab4\
-set dll=int2str
+set dll1=int2str
+set dll2=word_count
 set main=main
-tasm32 /ml /l %folder%%dll%.asm
+tasm32 /ml /l %folder%%dll1%.asm
 timeout 100
-tlink32 /Tpd /c %dll%.obj,,,,%folder%%dll%.def
+tlink32 /Tpd /c %dll1%.obj,,,,%folder%%dll1%.def
+timeout 100
+tasm32 /ml /l %folder%%dll2%.asm
+timeout 100
+tlink32 /Tpd /c %dll2%.obj,,,,%folder%%dll2%.def
 timeout 100
 tasm32 /ml /l %folder%%main%.asm
 timeout 100
+implib %dll2%.lib %dll2%.dll
 tlink32 /Tpe /aa /x /c %main%.obj
 timeout 100
 td32 %main%
--- lab4/main.asm
+++ lab4/main.asm
@@ -1,14 +1,20 @@
 include 	lab4\int2str.inc
+includelib	word_count.lib
+		extrn	word_count:near
+
 		.386
 		.model	FLAT,STDCALL
 
 		.const
-mb_title	db	'Lab 4',0
 libr		db	'int2str.dll',0
 nameproc	db	'int2str',0
-.data
-string		db	12 dup(?)
-number		dd	-1234567890
+mb_title	db	'String:'
+s		db	'  some  words with   spaces  ',0
+c		dd	' '
+		.data
+string		db	'Count: '
+count_str	db	12 dup(?)
+number		dd	0
 hlib		dd	?
 int2str		dd	?
 
@@ -18,7 +24,11 @@ _start:		call	LoadLibrary,offset libr
 		call	GetProcAddress,hlib,offset nameproc
 		mov	int2str,eax
 
-		call	int2str,number,offset string
+		call	word_count,offset s,c
+		movzx	eax,al
+		mov	number,eax
+
+		call	int2str,number,offset count_str
 		call	MessageBox,0,offset string,offset mb_title,MB_OK
 		call	ExitProcess,0
 	        ends
